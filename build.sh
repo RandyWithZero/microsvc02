258f799adf9a102ef7b7b828104e6e8fbe89c593
wangsq <wangsq@tongtech.com>
2021-03-01 18:21:40 +0800

    add Dockerfile

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 image="168.1.9.1/test/microsvc02:latest"
 docker build -t $image .
+docker push $image
--- controllers/svc.go
+++ controllers/svc.go
@@ -4,17 +4,23 @@ import (
 	"github.com/astaxie/beego"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
+
 const (
-	microSvcDb01Url ="http://svcdb:9091/api/svcdb"
+	microSvcDb01Url = "http://svcdb:9091/api/svcdb"
 )
+
 type MicroSVC02Controller struct {
 	beego.Controller
 }
+
 func (c *MicroSVC02Controller) Get() {
 	response, _ := http.Get(microSvcDb01Url)
 	defer response.Body.Close()
 	all, _ := ioutil.ReadAll(response.Body)
-	msg :="--->调用svc02"
 	msg2 := string(all)
-	c.Ctx.WriteString(msg+msg2)}
+	getEnv := os.Getenv("HOSTNAME")
+	msg := "--->调用svc02" + "[" + getEnv + "]"
+	c.Ctx.WriteString(msg + msg2)
+}
